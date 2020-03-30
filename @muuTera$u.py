9a5157fd6b3a4fc8a4457f4ecad2a61d28b1f313
Tripl0Color <62907882+Tripl0Color@users.noreply.github.com>
2020-03-30 22:58:04 +0300

    Add files via upload

---
+++ @muuTera$u.py
@@ -0,0 +1,38 @@
+#Use on py flask
+print ("""$$$$$$$$\        $$\           $$\        $$$$$$\            $$\                     
+\__$$  __|       \__|          $$ |      $$  __$$\           $$ |                    
+   $$ | $$$$$$\  $$\  $$$$$$\  $$ |      $$ /  \__| $$$$$$\  $$ | $$$$$$\   $$$$$$\  
+   $$ |$$  __$$\ $$ |$$  __$$\ $$ |      $$ |      $$  __$$\ $$ |$$  __$$\ $$  __$$\ 
+   $$ |$$ |  \__|$$ |$$ /  $$ |$$ |      $$ |      $$ /  $$ |$$ |$$ /  $$ |$$ |  \__|
+   $$ |$$ |      $$ |$$ |  $$ |$$ |      $$ |  $$\ $$ |  $$ |$$ |$$ |  $$ |$$ |      
+   $$ |$$ |      $$ |$$$$$$$  |$$ |      \$$$$$$  |\$$$$$$  |$$ |\$$$$$$  |$$ |      
+   \__|\__|      \__|$$  ____/ \__|$$$$$$\\______/  \______/ \__| \______/ \__|      
+                     $$ |          \______|                                          
+                     $$ |                                                            
+                     \__|    """)
+from flask import Flask, request, json
+import time
+import vk_api
+import random
+ 
+vk = vk_api.VkApi(token="paste your token vk group here")        #vk group token here
+ 
+app = Flask(__name__)
+ 
+@app.route('/', methods = ["POST"])
+def main():
+    data = json.loads(request.data)
+    if data["type"] == "confirmation":
+        return "paste code vk here"                               #code vk here
+    elif data["type"] == "message_new":
+        object = data["object"]
+        id = object["peer_id"]
+        body = object["text"]
+        if body.lower() == "hello":
+                vk.method("messages.send", {"peer_id": id, "message": "hi", "random_id": random.randint(1, 2147483647)})
+        
+        else:
+            vk.method("messages.send", {"peer_id": id, "message": "your msg", "random_id": random.randint(1, 2147483647)})
+    return "ok"
+
+                     
